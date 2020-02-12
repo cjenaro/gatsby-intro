92ca458e5cd5f240c75b30906e059a6e48076e48
Jenaro <jen.calvineo@gmail.com>
2020-02-12 18:17:07 -0300

    Added hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,20 @@ import Layout from '../components/layout';
 
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Hello</h1>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => {
-        return <PostPreview key={post.slug} post={post} />;
-      })}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => {
+          return <PostPreview key={post.slug} post={post} />;
+        })}
+      </Layout>
+    </>
   );
 };
